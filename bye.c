f76ac5809787800c0c9b8b00e2be13f37823ce4e
Ajay <shokalajaya@gmail.com>
2024-08-25 13:41:22 +0530

    Second commit

--- bye.c
+++ bye.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
 void main(){
+  Serial.printf("Martin branch");
   Serial.printf("Bye.C");
 }
--- hello.c
+++ hello.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
 void main(){
+  Serial.printf("Martin branch");
   Serial.printf("Hello.C");
 }
